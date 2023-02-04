3b63abc6add61da8f6af0c13c09c6151d67768d2
Sanyam Narang <sanyamnarang40@gmail.com>
2023-02-04 13:43:27 +0530

    Update App.java

--- 1 - Spring Core/Dependency Injection/Annotation Config/GreetingApp ( WithCoaching )/src/App.java
+++ 1 - Spring Core/Dependency Injection/Annotation Config/GreetingApp ( WithCoaching )/src/App.java
@@ -7,34 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	/*
-    	 * ---> THIS IS TIGHT COPULING
-		 * EnglishGreetingService eng = new EnglishGreetingService();
-		 * eng.greet("Sanyam");
-		 * 
-		 * FrenchGreetingService fre = new FrenchGreetingService(); fre.greet("Sanyam");
-		 * 
-		 * SpanishGreetingService spa = new SpanishGreetingService();
-		 * spa.greet("Sanyam");
-		 */
-    	
-    	// --> THIS IS LOSE COPULING
-    	/*Scanner sc=new Scanner(System.in);
-    	System.out.print("ENTER THE NAME : ");
-    	String name=sc.next();*/
-    	
-    	/* FactoryGreetingService factory = new FactoryGreetingService();
-    	Greeting_Service service = factory.greeting("fre");
-    	
-    	service.greet("Sanyam");
-    	
-    	FactoryFortune_Service factory2=new FactoryFortune_Service();
-    	Fortune_Teller service2=factory2.Fortune("fre");
-    	service2.tellFortune(); */
-    	
-    	// --> SPRING CODE - USING DEPENDENCY INJECTION - XML & ANNOTATION BASED 
+    public static void main( String[] args ){
+    	// --> SPRING CODE - ANNOTATION BASED 
     	
     	// Step 1 : Initialize the Spring Container
     	//ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
